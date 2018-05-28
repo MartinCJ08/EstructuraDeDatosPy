ed5d5b80eae9e56e3a5cbccce12c7d45ea5de5d2
MartinCJ08 <martin.carracoj@gmail.com>
2018-05-28 02:15:17 -0500

    Add Queue class

---
+++ Queue.py
@@ -0,0 +1,20 @@
+from DoubleNode import *
+class Queue:
+	def __init(self):
+		self.top = None
+		self.final = None
+	def queue(self,data):
+		#add an element to the queue
+		if self.top == None:
+			self.top = NodeQueue()
+			self.top.setData(data)
+			self.final = self.top
+		else:
+			pass
+		
+	def desqueue(self):
+		#remove de first element from the queue
+		pass
+	def consult(self):
+		#Consult the first element from the queue
+		pass
